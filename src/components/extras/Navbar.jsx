964b179f3088246232aabfc0862d2a4122a35261
Nacho1593 <jcarricart@gmail.com>
2021-05-18 17:27:28 -0300

    upadate

--- src/components/extras/Navbar.jsx
+++ src/components/extras/Navbar.jsx
@@ -31,7 +31,7 @@ function Navbar() {
                 placeholder="Search"
                 aria-label="Search"
               />
-              <button className="btn btn-outline-success" type="submit">
+              <button className="btn btn-outline-secondary" type="submit">
                 Search
               </button>
             </form>
